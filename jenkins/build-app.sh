e53eb06a53325217eaa399e1ce9bdc389acf6859
Sofus Albertsen <soa@praqma.com>
2019-12-12 18:04:01 +0100

    Change due to dod is not happy about mounts

--- jenkins/build-app.sh
+++ jenkins/build-app.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-#app/gradlew shadowjar -p app
-docker run --rm -u gradle -v "$PWD"/app:/home/gradle/project -w /home/gradle/project gradle:jdk11 gradle clean shadowjar
+gradle clean shadowjar -p app
